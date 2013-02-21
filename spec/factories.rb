7520b75f0f160fff20fc27b50766b5cd023bdede
Matthew Colson <colson78@live.missouristate.edu>
2013-02-21 06:37:25 -0600

    gravitar

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Matthew Colson"
+    email    "matthew_colson@hotmail.com"
+    password "Lego1942"
+    password_confirmation "Lego1942"
+  end
+end
