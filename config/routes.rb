7bbfacd4e445656b0b92b8722646ae258323abce
Matthew Colson <colson78@live.missouristate.edu>
2013-02-21 16:39:06 -0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SampleApp::Application.routes.draw do
   resources :users
 
   root to: 'static_pages#home'
-  match '/signup', to: 'user#new'
+  match '/signup', to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
